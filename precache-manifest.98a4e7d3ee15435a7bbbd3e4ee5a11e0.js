c5e0c0877c953b0a542c866408f7ccc0863fc27e
vishakha19mehta <45995080+vishakha19mehta@users.noreply.github.com>
2020-07-20 12:01:50 +0700

    Updates

---
+++ precache-manifest.98a4e7d3ee15435a7bbbd3e4ee5a11e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76db83b175a60d0f46cd2814f421064",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "e999a0ec41302beca7bf",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/myapp/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e999a0ec41302beca7bf",
+    "url": "/myapp/static/js/main.bed5d1c0.chunk.js"
+  },
+  {
+    "revision": "dfc084a7ee4f2eebf7cd",
+    "url": "/myapp/static/js/runtime-main.5f0af923.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
